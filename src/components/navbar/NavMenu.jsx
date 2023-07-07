e8c49910c1d546a0a02140548359635ed573fc86
haider-patanwala <haiderpatanwala@hotmail.com>
2023-07-08 01:51:44 +0530

    Add Mobile Navbar

--- src/components/navbar/NavMenu.jsx
+++ src/components/navbar/NavMenu.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { CgMenuRight, CgClose } from 'react-icons/cg';
+import NavMobile from './NavMobile';
 
 const navigations = [
 	{
@@ -21,19 +23,39 @@ const navigations = [
 ];
 
 const NavMenu = () => {
+	const [mobileNav, setMobileNav] = useState(false);
 	return (
-		<nav className='md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center'>
-			{navigations.map((navigation) => {
-				return (
-					<Link
-						to={navigation.path}
-						className='mr-5 hover:text-gray-900'
-					>
-						{navigation.name}
-					</Link>
-				);
-			})}
-		</nav>
+		<>
+			<div
+				onClick={() => setMobileNav(!mobileNav)}
+				className='text-2xl md:hidden lg:text-3xl cursor-pointer'
+			>
+				{mobileNav ? <CgClose /> : <CgMenuRight />}
+			</div>
+			<nav className='hidden md:ml-auto md:mr-auto md:flex md:flex-wrap md:items-center text-base md:justify-center'>
+				<ul className='md:flex md:gap-x-8'>
+					{navigations.map((navigation) => {
+						return (
+							<li>
+								<Link
+									to={navigation.path}
+									className='mr-5 capitalize hover:border-b transition-all hover:text-gray-900'
+								>
+									{navigation.name}
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
+			<div
+				className={`${
+					mobileNav ? 'left-0' : '-left-full'
+				} md:hidden fixed bottom-0 w-full max-w-xs h-screen transition-all`}
+			>
+				<NavMobile />
+			</div>
+		</>
 	);
 };
 
