656b94dc1b3a1bac4799140da3393419b3a7d199
Riyadcy <riyad9934@gmail.com>
2023-07-17 01:23:38 +0530

    Changed CompanyListings

--- src/modules/CompanyListings.jsx
+++ src/modules/CompanyListings.jsx
@@ -3,7 +3,7 @@ import CompanyList from '../components/CompanyList';
 
 const CompanyListings = () => {
 	return (
-<div className='flex flex-col justify-center w-full'>
+		<div className='flex flex-col justify-center w-full'>
 			{/*------------- Background Gradient ------------ */}
 			<div className='gradient z-0'></div>
 
@@ -20,26 +20,56 @@ const CompanyListings = () => {
 					className=' blue-gradient text-center text-3xl md:text-4xl
 				 font-semibold'
 				>
-					Explore Companies
+					Company reviews. Salaries. Interviews. Jobs.
 				</h1>
 			</div>
 
+			{/*------------- Search Bar ------------*/}
+			{/* <div className="flex justify-center my-2 relative mx-3">
+				<form className='flex lg:w-3/5 flex-col justify-center w-full md:w-4/5'>
+					<label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+						Search
+					</label>
+					<div className="relative">
+						<div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+							<svg
+								className="w-4 h-4 text-gray-500 dark:text-gray-400"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 20 20"
+							>
+								<path
+									stroke="currentColor"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+								/>
+							</svg>
+						</div>
+						<input
+							type="search"
+							id="default-search"
+							className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							placeholder="Search Companies..."
+							required
+						/>
+						<button
+							type="submit"
+							className="text-white absolute right-2.5 bottom-2.5 bg-accent hover:bg-slate-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+						>
+							Search
+						</button>
+					</div>
+				</form>
+			</div> */}
+
 			<div className='flex justify-center my-6 relative mx-3'>
 				<div className='flex lg:w-3/5 flex-col justify-center w-full md:w-4/5 items-start border z-10 border-slate-300  bg-white/50 rounded-2xl py-5'>
-					<h1 className='text-2xl text-start font-medium text-slate-800 px-5'>
-					List of companies
+					<h1 className='text-2xl text-start font-medium text-slate-800 px-5 my-2'>
+						List of companies
 					</h1>
-					<div className='flex mt-6 w-full border-b '>
-						<div className='tabs'>
-							<a className='tab tab-bordered tab-active '>Open Projects</a>
-							<a className='tab'>Closed Projects</a>
-						</div>
-					</div>
-
-					<p className='mx-5 my-2 text-base'>
-						Browse projects that match your experience to a client's hiring
-						preferences. Ordered by most relevant.
-					</p>
 					<CompanyList />
 					<CompanyList />
 					<CompanyList />
