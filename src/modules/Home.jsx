ac9884da444bc9f31374c273baee411caa76b498
haider-patanwala <haiderpatanwala@hotmail.com>
2023-07-18 18:03:49 +0530

    API Integration

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Featured from '../components/Featured';
+import Footer from '../components/Footer';
 
 const Home = () => {
 	return (
