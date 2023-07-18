6790a405cb9c1fd1cc5f7506b5df965c055d3e8f
Riyadcy <riyad9934@gmail.com>
2023-07-19 01:13:10 +0530

    Fetching Organisation from backend using id

--- src/modules/CompanyMain.jsx
+++ src/modules/CompanyMain.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import CompanyDetails from '../components/CompanyDetails';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-const CompanyMain = () => {
-	return <div>CompanyMain</div>;
+function CompanyMain() {
+	const [organization, setOrganization] = useState([]);
+	const { uid } = useParams();
+	useEffect(() => {
+		// const urlParams = new URLSearchParams(window.location.search);
+		// const myParam = urlParams.get('id');
+		console.log("param is ---------", uid);
+	
+		const fetchorganization = async () => {
+			const response = await fetch(
+				`https://projekto-backend.onrender.com/organizations/${uid}`,
+				{ mode: 'cors' }
+			);
+			const fetchedOrganization = await response.json();
+			setOrganization(fetchedOrganization.data);
+			console.log('fetch organization------------', fetchedOrganization.data);
+			// console.log('fetch organization------------', organization);
+		};
+		fetchorganization();
+	}, []);
+
+	return(
+		<div>
+			<CompanyDetails org_data={organization}/>	
+		</div>
+	)
 };
 
 export default CompanyMain;
