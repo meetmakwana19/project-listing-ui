123cd8bcfa5d5367d5ab018a60bf00ac53e82005
haider-patanwala <haiderpatanwala@hotmail.com>
2023-07-19 02:39:37 +0530

    Add Register Form

---
+++ src/components/form/register/steps/AccountCredentials.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+
+const AccountCredentials = () => {
+	// 1.Form
+	// fName lname
+	// Email
+	// Password
+	// Re-enter Password
+
+	return (
+		<div className='w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8'>
+			<div className='flex flex-auto gap-5 w-full items-center justify-between'>
+				<div className='relative w-full'>
+					<p
+						className='bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
+								  absolute'
+					>
+						First Name
+					</p>
+					<input
+						placeholder='John'
+						type='text'
+						className='border capitalize placeholder-gray-400 focus:outline-none
+								  focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+								  border-gray-300 rounded-md'
+					/>
+				</div>
+				<div className='relative w-full'>
+					<p
+						className='bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
+								  absolute'
+					>
+						Last Name
+					</p>
+					<input
+						placeholder='John'
+						type='text'
+						className='border capitalize placeholder-gray-400 focus:outline-none
+								  focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+								  border-gray-300 rounded-md'
+					/>
+				</div>
+			</div>
+			<div className='relative'>
+				<p className='bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600 absolute'>
+					Email
+				</p>
+				<input
+					placeholder='johndoe@example.com'
+					type='text'
+					className='border lowercase placeholder-gray-400 focus:outline-none
+                  focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+                  border-gray-300 rounded-md'
+				/>
+			</div>
+			<div className='relative'>
+				<p
+					className='bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
+                  absolute'
+				>
+					Password
+				</p>
+				<input
+					placeholder='Password'
+					type='password'
+					className='border placeholder-gray-400 focus:outline-none
+                  focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+                  border-gray-300 rounded-md'
+				/>
+			</div>
+			<div className='relative'>
+				<p
+					className='bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
+                  absolute'
+				>
+					Re-enter Password
+				</p>
+				<input
+					placeholder='Password'
+					type='password'
+					className='border placeholder-gray-400 focus:outline-none
+                  focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+                  border-gray-300 rounded-md'
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default AccountCredentials;
--- src/modules/Register.jsx
+++ src/modules/Register.jsx
@@ -1,103 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Stepper from '../components/form/register/Stepper';
+import StepperControl from '../components/form/register/StepperControl';
+import { UseContextProvider } from '../components/form/register/StepperContext';
+import AccountCredentials from '../components/form/register/steps/AccountCredentials';
+import PersonalInfo from '../components/form/register/steps/PersonalInfo';
+import Final from '../components/form/register/steps/Final';
+import FormContainer from '../components/form/FormContainer';
 
 const Register = () => {
+	const [currentStep, setCurrentStep] = useState(1);
+
+	const steps = ['Login Details', 'Personal Details', 'Review'];
+
+	const displayStep = (step) => {
+		switch (step) {
+			case 1:
+				return <AccountCredentials />;
+			case 2:
+				return <PersonalInfo />;
+			case 3:
+				return <Final />;
+			default:
+		}
+	};
+
+	const handleClick = (direction) => {
+		let newStep = currentStep;
+		console.log('newstep---', newStep);
+		console.log('lenght?------', steps.length);
+
+		direction === 'next' ? newStep++ : newStep--;
+		// check if steps are within bounds
+		newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+	};
+
 	return (
-		<div className='flex justify-center items-center h-[80vh]'>
-			<div className='w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700'>
-				<form
-					className='space-y-6'
-					action='#'
-				>
-					<h5 className='text-xl font-medium text-gray-900 dark:text-white'>
-						Sign Up to our platform
-					</h5>
-					<div>
-						<label
-							for='email'
-							className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-						>
-							Your email
-						</label>
-						<input
-							type='email'
-							name='email'
-							id='email'
-							className='bg-gray-50 border border-gray-300
-							 text-gray-900 text-sm rounded-lg focus:ring-blue-500
-							  focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600
-							   dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
-							placeholder='name@company.com'
-							required
+		<div
+		//  className='mx-auto rounded-2xl bg-white pb-2 shadow-xl md:w-1/2'
+		>
+			{/* Stepper */}
+			<FormContainer>
+				<div className='relative h-full w-full'>
+					<div className='w-full h-full relative'>
+						<Stepper
+							steps={steps}
+							currentStep={currentStep}
 						/>
+						<UseContextProvider>{displayStep(currentStep)}</UseContextProvider>
 					</div>
-					<div>
-						<label
-							for='password'
-							className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-						>
-							Your password
-						</label>
-						<input
-							type='password'
-							name='password'
-							id='password'
-							placeholder='••••••••'
-							className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
-							required
+					{/* navigation button */}
+					<div className=' w-full absolute bottom-0'>
+						{/* {currentStep !== steps.length && ( */}
+						<StepperControl
+							handleClick={handleClick}
+							currentStep={currentStep}
+							steps={steps}
 						/>
+						{/* )} */}
 					</div>
-					<div>
-						<label
-							for='confirm_password'
-							className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-						>
-							Re-enter password
-						</label>
-						<input
-							type='password'
-							name='confirm_password'
-							id='confirm_password'
-							placeholder='••••••••'
-							className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
-							required
-						/>
-					</div>
-					<div className='flex items-start'>
-						<div className='flex items-start'>
-							<div className='flex items-center h-5'>
-								<input
-									id='remember'
-									type='checkbox'
-									value=''
-									className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800'
-									required
-								/>
-							</div>
-							<label
-								for='newsletters'
-								className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-							>
-								Get weekly newsletters
-							</label>
-						</div>
-					</div>
-					<button
-						type='submit'
-						className='w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-					>
-						Register
-					</button>
-					<div className='text-sm font-medium text-gray-500 dark:text-gray-300'>
-						Already registered?{' '}
-						<a
-							href='/login'
-							className='text-blue-700 hover:underline dark:text-blue-500'
-						>
-							Sign In
-						</a>
-					</div>
-				</form>
-			</div>
+				</div>
+			</FormContainer>
 		</div>
 	);
 };
