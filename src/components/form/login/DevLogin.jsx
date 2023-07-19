9ce6bc70ae27d2917a869a1891c8b0323199136b
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-19 12:06:37 +0530

    Login API integerated

--- src/components/form/login/DevLogin.jsx
+++ src/components/form/login/DevLogin.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LoginContainer } from './LoginContainer';
 import developer from '/developer.svg';
 
 export const DevLogin = () => {
+	const [form, setForm] = useState({
+		email: "",
+		password: "",
+	})
+	console.log("Dev login----", form);
+
+	const onSignIn = () => {
+		fetch("https://projekto-backend.onrender.com/developers/auth/login", {
+			method: "POST",
+			headers: {
+				"Content-Type":"application/json",
+			},
+			body: JSON.stringify(form)
+		})
+		.then((response) => response.json())
+		.then((data) => {
+			console.log("LOGGED IN --> ", data);
+		})
+		.catch((error) => {
+			console.log("POSTING error --> ", error);
+		})
+
+	}
+	
 	return (
 		<LoginContainer image={developer}>
 			<div className='w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8'>
@@ -13,6 +37,7 @@ export const DevLogin = () => {
 					<input
 						placeholder='johndoe@example.com'
 						type='text'
+						value={form.email} onChange={(e) => setForm({...form, email: e.target.value})}
 						className='border lowercase placeholder-gray-400 focus:outline-none
           focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
           border-gray-300 rounded-md'
@@ -28,13 +53,14 @@ export const DevLogin = () => {
 					<input
 						placeholder='Password'
 						type='password'
+						value={form.password} onChange={(e) => setForm({...form, password: e.target.value})}
 						className='border placeholder-gray-400 focus:outline-none
           focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
           border-gray-300 rounded-md'
 					/>
 				</div>
 				<button
-					onClick={() => handleClick('next')}
+					onClick={() => onSignIn()}
 					className={`absolute -bottom-52 cursor-pointer  pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-indigo-500
         rounded-lg transition duration-200 hover:bg-indigo-600 ease w-full`}
 				>
