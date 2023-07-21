64738f3bac691302d8eaed8ec39b58c8d74b9d4f
haider-patanwala <haiderpatanwala@hotmail.com>
2023-07-21 21:12:50 +0530

    added radius to search and padding in tabs

--- src/components/navbar/Search.jsx
+++ src/components/navbar/Search.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const Search = ({searchInput, setSearchInput}) => {
+const Search = ({ searchInput, setSearchInput }) => {
 	const onSearch = (event) => {
 		event.preventDefault();
-		setSearchInput({...searchInput, searchString : event.target.value})
-	}
+		setSearchInput({ ...searchInput, searchString: event.target.value });
+	};
 	return (
 		<div className='flex gap-0 items-center w-full rounded-xl p-0 h-10 justify-center'>
 			<input
 				type='text'
-				placeholder='Type here'
+				placeholder='Type here to search...'
 				value={searchInput.searchString}
 				onChange={(e) => onSearch(e)}
-				className='px-3 h-full outline-1 border w-4/5 rounded-l-xl border-slate-300 outline-slate-300/50 '
+				className='px-3 h-full outline-1 border w-full rounded-xl text-center border-slate-300 outline-slate-300/50 '
 			/>
 			{/* <button className='flex items-center justify-center bg-accent px-3 h-full w-1/5 rounded-r-xl text-white font-medium' onClick={searchClick}>
 				Search
