8d194f178b1b0ba9304d2af6d3c1886e23f89f80
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-24 02:52:01 +0530

    Remjoved logs

--- src/components/profile/OrgProfile.jsx
+++ src/components/profile/OrgProfile.jsx
@@ -1,35 +1,33 @@
-import React from 'react';
-import CompanyDetails from '../components/CompanyDetails';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import CompanyDetails from '../components/CompanyDetails';
 
-const OrgProfile = () => {
-	const [organization, setOrganization] = useState([]);
-	const { uid } = useParams();
-	useEffect(() => {
-		// const urlParams = new URLSearchParams(window.location.search);
-		// const myParam = urlParams.get('id');
-		console.log('param is ---------', uid);
+function OrgProfile() {
+  const [organization, setOrganization] = useState([]);
+  const { uid } = useParams();
+  useEffect(() => {
+    // const urlParams = new URLSearchParams(window.location.search);
+    // const myParam = urlParams.get('id');
+    console.log('param is ---------', uid);
 
-		const fetchorganization = async () => {
-			const response = await fetch(
-				`https://projekto-backend.onrender.com/organizations/${uid}`,
-				{ mode: 'cors' }
-			);
-			const fetchedOrganization = await response.json();
-			setOrganization(fetchedOrganization.data);
-			console.log('fetch organization------------', fetchedOrganization.data);
-			// console.log('fetch organization------------', organization);
-		};
-		fetchorganization();
-	}, []);
+    const fetchorganization = async () => {
+      const response = await fetch(
+        `https://projekto-backend.onrender.com/organizations/${uid}`,
+        { mode: 'cors' },
+      );
+      const fetchedOrganization = await response.json();
+      setOrganization(fetchedOrganization.data);
+      // console.log('fetch organization------------', fetchedOrganization.data);
+      // console.log('fetch organization------------', organization);
+    };
+    fetchorganization();
+  }, []);
 
-	return (
-		<div>
-			<CompanyDetails org_data={organization} />
-		</div>
-	);
-};
+  return (
+    <div>
+      <CompanyDetails org_data={organization} />
+    </div>
+  );
+}
 
 export default OrgProfile;
