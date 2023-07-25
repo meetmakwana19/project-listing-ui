336fb990acf7a47aa07c5fac0932e4541c720a60
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-25 19:05:37 +0530

    Added conditional rendering for org proposals

--- src/components/CompanyDetails.jsx
+++ src/components/CompanyDetails.jsx
@@ -2,10 +2,15 @@
 // import { BiSolidMap } from 'react-icons/bi';
 
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function CompanyDetails({ org_data, update, edit }) {
   const [orgProposals, setOrgProposals] = useState([]);
+
+  // it will get empty object for /profile page
+  // but it will get {uid : xxx} object for /companies/:uid page
+  const profile = useParams();
+
   useEffect(() => {
     const orgId = localStorage.getItem("isOrg");
     fetch(
@@ -130,52 +135,58 @@ function CompanyDetails({ org_data, update, edit }) {
           </div>
         </div>
       </div>
-      <div
-        className="flex w-full lg:w-3/5 md:w-4/5 flex-col justify-center
+      {/* only render if the url param has {} object due to no :uid in url */}
+      {!profile.uid === true
+        ? (
+          <div
+            className="flex w-full lg:w-3/5 md:w-4/5 flex-col justify-center
             items-center border z-10 relative
            border-slate-300  bg-white/50 rounded-2xl my-6 mb-10"
-      >
-        <div className="flex flex-col px-5 py-7">
-          <h1 className="text-2xl font-semibold mb-3">Company&apos;s Projects Proposals</h1>
-          {/* ---------TODO: Comapny Projects------------ */}
-          <div className="border-b py-5 border-slate-300 ">
-            {orgProposals && orgProposals.map((project) => (
-              <div className="flex items-center my-5" key={project.uid}>
-                <img
-                  src={project.project.thumbnail}
-                  alt=""
-                  className="w-[10vw] md:w-20 rounded-full  object-cover aspect-square mr-[-15%]"
-                />
-                <Link to={`/developers/${project.developer.uid}`}>
-                  <img
-                    src={project.developer.profile_pic}
-                    alt=""
-                    className="w-[10vw] md:w-20 rounded-full  object-cover aspect-square mx-8"
-                  />
-                </Link>
-                <div>
-                  <Link to={`/projects/${project.project.uid}`} className="text-xl font-semibold mt-3 hover:text-accent">{project.project.title}</Link>
-                  <div className="flex place-content-start items-center w-full text-slate-600 gap-1">
-                    {/* ------------------------ Developer City-------------------------- */}
-                    <p>{project.project.uid}</p>
+          >
+            <div className="flex flex-col px-5 py-7">
+              <h1 className="text-2xl font-semibold mb-3">Company&apos;s Projects Proposals</h1>
+              {/* ---------TODO: Comapny Projects------------ */}
+              <div className="border-b py-5 border-slate-300 ">
+                {orgProposals && orgProposals.map((project) => (
+                  <div className="flex items-center my-5" key={project.uid}>
+                    <img
+                      src={project.project.thumbnail}
+                      alt=""
+                      className="w-[10vw] md:w-20 rounded-full  object-cover aspect-square mr-[-15%]"
+                    />
+                    <Link to={`/developers/${project.developer.uid}`}>
+                      <img
+                        src={project.developer.profile_pic}
+                        alt=""
+                        className="w-[10vw] md:w-20 rounded-full  object-cover aspect-square mx-8"
+                      />
+                    </Link>
+                    <div>
+                      <Link to={`/projects/${project.project.uid}`} className="text-xl font-semibold mt-3 hover:text-accent">{project.project.title}</Link>
+                      <div className="flex place-content-start items-center w-full text-slate-600 gap-1">
+                        {/* ------------------------ Developer City-------------------------- */}
+                        <p>{project.project.uid}</p>
+                      </div>
+                      <p className="description mb-4">
+                        Proposal :
+                        {' '}
+                        {project.uid}
+                        {' '}
+                        by
+                        {' '}
+                        {project.developer.fname}
+                        {" "}
+                        {project.developer.lname}
+                      </p>
+                    </div>
                   </div>
-                  <p className="description mb-4">
-                    Proposal :
-                    {' '}
-                    {project.uid}
-                    {' '}
-                    by
-                    {' '}
-                    {project.developer.fname}
-                    {" "}
-                    {project.developer.lname}
-                  </p>
-                </div>
+                ))}
               </div>
-            ))}
+            </div>
           </div>
-        </div>
-      </div>
+        )
+        : null }
+
     </div>
   );
 }
