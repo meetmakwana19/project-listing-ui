78c49b7982567fd85d0047cb44fdff4e439e6fb9
meetmakwana19 <meetmakwana203@gmail.com>
2023-08-18 17:53:23 +0530

    Removed re-enter pw

--- src/components/form/register/developer/AccountCredentials.jsx
+++ src/components/form/register/developer/AccountCredentials.jsx
@@ -75,7 +75,7 @@ function AccountCredentials({ formData, setFormData }) {
                   border-gray-300 rounded-md"
         />
       </div>
-      <div className="relative">
+      {/* <div className="relative">
         <p
           className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
                   absolute"
@@ -92,7 +92,7 @@ function AccountCredentials({ formData, setFormData }) {
                   focus:border-accent w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                   border-gray-300 rounded-md"
         />
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/form/register/organization/OrgAccount.jsx
+++ src/components/form/register/organization/OrgAccount.jsx
@@ -39,7 +39,7 @@ function OrgAccount({ formData, setFormData }) {
           border-gray-300 rounded-md"
         />
       </div>
-      <div className="relative">
+      {/* <div className="relative">
         <p
           className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
           absolute"
@@ -57,6 +57,7 @@ function OrgAccount({ formData, setFormData }) {
           border-gray-300 rounded-md"
         />
       </div>
+      ... */}
     </div>
   );
 }
